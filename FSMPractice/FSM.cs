301e5538810faf5541f275eef2d57a0b461aa9b9
BlaneTorregrossa <blanet97@cox.net>
2017-01-26 17:53:26 -0600

    Initial Commit

---
+++ FSMPractice/FSM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FSMPractice
+{
+    class FSM
+    {
+
+        CallBack onTest;
+        CallBack onAltTest;
+
+        public FSM()
+        {
+
+            onTest = null;
+
+        }
+
+        public void AddStuffToTest(CallBack T, CallBack A)
+        {
+            onTest += T;
+            onAltTest += A;
+        }
+
+        public void Test()
+        {
+            
+
+        }
+
+
+        public void AltTest()
+        {
+
+
+        }
+
+
+        //Dictionary<String, State> states;
+        //State aState; /*State bState; State cState; State dState; State eState;*/
+
+
+        //public void StateChange(State s)
+        //{
+        //    aState.exitState();
+        //    // aState.Equals();
+        //    aState = s;
+        //    //s.enterState();
+        //    aState.enterState();
+
+        //}
+
+
+        //public void StateTransistions()
+        //{
+
+        //}
+
+        
+
+        //public bool Start()
+        //{
+        //    return true;
+        //}
+
+    }
+}
---
+++ FSMPractice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ 
+            INIT transitions to NOWIND upon the condition of an input of s
+            NOWIND transitions to LIGHTWIND upon the condition of an input of l
+            NOWIND transitions to EXTREMEWIND upon the condition of an input of e
+            LIGHTWIND transitions to NOWIND upon the condition of an input of n
+            LIGHTWIND transitions to EXTEMEWIND upon the condition of an input of e
+            EXTREMEWIND transitions to LIGHTWIND upon the condition of an input of l
+            EXTREMEWIND transitions to NOWIND upon the condition of an input of n
+            EXTREMEWIND transitions to END upon the condition of an input of q
+     
+ */
+
+
+namespace FSMPractice
+{
+
+
+    delegate void CallBack();
+
+
+    class Program
+    {
+
+        int NumA = 25;
+        int NumB = 256;
+        int NumC = 8;
+        int sum;
+        string WordA = "Hello!";
+
+
+        static void Main(string[] args)
+        {
+            FSM test = new FSM();
+
+
+        }
+
+
+        static public void ActionA()
+        {
+            Console.Write("I have ants in my eyes.");
+        }
+
+
+        static public void ActionB()
+        {
+            
+        }
+
+
+        static public void ActionC()
+        {
+
+        }
+
+
+        static public void ActionD()
+        {
+
+        }
+
+        static public void ActionE()
+        {
+
+        }
+
+    }
+}
